a9201e1e64bf28b3ee0bd3d33ec8685ec7966513
Marta233 <martaassefa23@gmail.com>
2023-04-19 22:16:23 +0300

    full

--- Server/Server/Answer/AnswerRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createanswer, getAnswer } = require("./AnswerController");
-
-router.post("/", createanswer);
-router.get("/:quastion_id", getAnswer);
-
-module.exports = router;
